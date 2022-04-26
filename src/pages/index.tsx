fe7a8fe5c45fd7d76ead26593e670d05770582e4
jinho1011 <gino9940@gmail.com>
2022-04-26 17:21:31 +0900

    Add hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from "next";
+import { usePopularQuery } from "../hooks/usePopularQuery";
+import { useTrendingQuery } from "../hooks/useTrendingQuery";
 
 const Home: NextPage = () => {
+  const trendings = useTrendingQuery("all", "week");
+  const populars = usePopularQuery();
+
   return <div>Hi</div>;
 };
 
