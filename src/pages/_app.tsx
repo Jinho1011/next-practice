611dc92d1ac403c150ed006acc1d72270fa06a34
jinho1011 <gino9940@gmail.com>
2022-04-26 17:21:26 +0900

    Add QueryClient

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from "next/app";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
