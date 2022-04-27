9e53cd20dba983e10b51f8dc6be51c0d9db09e3d
jinho1011 <gino9940@gmail.com>
2022-04-28 02:05:38 +0900

    Remove Error, Invalid condition

--- src/api/tmdb/trending.ts
+++ src/api/tmdb/trending.ts
@@ -1,11 +1,10 @@
 import { TMDB_KEY } from "../key";
 import { fetchTrendingResponse } from "../../interfaces/tmdb/trending";
-import { fetchTmdbError, fetchTmdbInvalid } from "../../interfaces/tmdb";
 
 export const fetchTrending = async (
   media_type: "all" | "movie" | "tv" | "person",
   time_window: "day" | "week"
-): Promise<fetchTrendingResponse | fetchTmdbError | fetchTmdbInvalid> => {
+): Promise<fetchTrendingResponse> => {
   const url = `https://api.themoviedb.org/3/trending/${media_type}/${time_window}?api_key=${TMDB_KEY}`;
   const response = await fetch(url);
   return await response.json();
